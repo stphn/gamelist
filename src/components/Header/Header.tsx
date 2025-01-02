e39d4bc090269bd365a6dd9476384d3b3ec39414
stphn <sgoeuriot@gmail.com>
2025-01-02 09:14:42 +0100

    added interface

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,11 +3,19 @@ import styles from './Header.module.css'
 import Logo from '../Logo'
 import ThemeToggle from './../ThemeToggle'
 
-const Header: React.FC = () => {
+interface HeaderProps {
+  theme: string
+  toggleTheme: () => void
+}
+
+const Header: React.FC<HeaderProps> = ({ theme, toggleTheme }) => {
   return (
     <header className={styles.header}>
       <Logo />
-
+      <h1>Game List</h1>
+      <button onClick={toggleTheme}>
+        Switch to {theme === 'light' ? 'dark' : 'light'} mode
+      </button>
       <ThemeToggle />
     </header>
   )
